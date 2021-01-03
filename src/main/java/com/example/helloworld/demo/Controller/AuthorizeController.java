1e7aea82164adcd2e89d00298ffa246adb0bde1b
nablevan <lkx10146@126.com>
2021-01-03 11:56:38 +0800

    利用session保存登录态 利用th：if控制内容显示

--- src/main/java/com/example/helloworld/demo/Controller/AuthorizeController.java
+++ src/main/java/com/example/helloworld/demo/Controller/AuthorizeController.java
@@ -1,13 +1,17 @@
 package com.example.helloworld.demo.Controller;
 
 
+import com.alibaba.fastjson.JSON;
 import com.example.helloworld.demo.Provider.GithubProvider;
 import com.example.helloworld.demo.dto.AccessTokenDTO;
 import com.example.helloworld.demo.dto.GithubUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
@@ -15,17 +19,30 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code")String code, @RequestParam(name = "state")String state){
+    public String callback(@RequestParam(name = "code")String code, @RequestParam(name = "state")String state, HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("6a312e03edabef8a3323");
-        accessTokenDTO.setClient_secret("3cccdaca866020e18d5a2ec632955a44fe89418d");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null){      //登陆成功
+            request.getSession().setAttribute("uesr", user);
+            System.out.println(user.getName());
+            return "redirect:/";    //redirect应以路径重定向，故不能redirect：index
+        }else {                 //登陆失败，显示失败信息
+            return "redirect:/";
+        }
+//        return "index";       //直接return index，地址栏会看到token，故重定向
     }
 }
