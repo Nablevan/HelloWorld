eec5612fae2c2a185d9cd65c7bb169e01913f5e0
nablevan <lkx10146@126.com>
2021-02-09 19:50:41 +0800

    添加未登录回复时提示登录，新开页面登录并自动关闭

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -10,10 +10,18 @@ function postComment() {
             "type": 1
         }),
         success: function (response) {
-            if (response.code == 200){
+            if (response.code == 200) {
                 $("#comment_section").hide();
-            }else {
-                alert(response.message)
+            } else {
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=6a312e03edabef8a3323&redirect_uri=http://localhost:8080/callback&scope=user&state=666")
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
             console.log(response)
         },
